a0b0dfed79efd5a4973c19b1e82e6a0555a8bc00
Ashwanth <iamashwanth@gmail.com>
2018-01-28 00:34:38 +0530

    KMP: cleanup and added comments

--- src/strings/kmp.cpp
+++ src/strings/kmp.cpp
@@ -1,3 +1,28 @@
+/* * * * * * * * * * * * * * * * *
+ * Knuth-Morris-Pratt algorithm  *
+ * * * * * * * * * * * * * * * * */
+
+/*
+
+   This algorithm efficiently matches a given pattern in any input string with
+   a time complexity of O(m+n), where m and n are the lenghts of pattern and the
+   input string respectively.
+
+   It achieves this by building a pattern table(like the one below) where each
+   cell stores the lenght of longest suffix that is also the prefix in the
+   substring upto that index.
+
+   --------------------------
+    a b c d a b c e a b c d e
+    0 0 0 0 1 2 3 0 1 2 3 4 0
+   --------------------------
+
+   During the search whenever there is a mismatch we need not begin our search
+   from the beginning of the pattern, but instead we can make use of the pattern
+   table above and move to the next smaller suffix(match) until we hit index 0.
+
+ */
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -11,34 +36,41 @@ class kmp {
 
 	kmp() {
 		len = 0;
+		table = NULL;
+	}
+
+	~kmp() {
+		delete table;
 	}
 
 	kmp(const std::string& pattern) {
-		len = 0;
 		set_match_pattern(pattern);
 	}
 
 	void print_pattern_table() {
 		for (int i=0; i<len; i++)
 			cout << "--";
-		cout << endl;
+		cout << endl << " ";
 		for (int i=0; i<len; i++)
 			cout << pattern[i] << " ";
-		cout << endl;
+		cout << endl << " ";
 		for (int i=0; i<len; i++)
 			cout << table[i] << " ";
 		cout << endl;
+		for (int i=0; i<len; i++)
+			cout << "--";
+		cout << endl;
+		cout << endl;
 	}
 
-	bool search_str(const std::string &str) {
+	int search_str(const std::string &str) {
 		int i=0, j=0;
 
 		while (i<str.length()) {
-			if (j == len) return true;
+			if (j == len) return i-len;
 
 			if (str[i] == pattern[j]) {
-				j++;
-				i++;
+				i++, j++;
 			} else if (j == 0) {
 				i++;
 			} else {
@@ -46,7 +78,7 @@ class kmp {
 			}
 		}
 
-		return false;
+		return -1;
 	}
 
 	private:
@@ -56,7 +88,7 @@ class kmp {
 	int len;
 
 	void build_pattern_table() {
-		int i=0, j=0;
+		int i=1, j=0;
 
 		while (i < len) {
 			if (pattern[i] == pattern[j]) {
