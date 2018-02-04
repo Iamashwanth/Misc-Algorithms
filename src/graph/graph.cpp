00daa8c8db86a331c7850e7dddb6396679962211
Ashwanth <iamashwanth@gmail.com>
2018-02-04 16:52:37 +0530

    graph: topolocial sort, shortest path
    TODO:
    Need to move definitions to headers
    Add inheritance for DFS, BFS and topological sort

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -3,6 +3,8 @@
 #include <tuple>
 #include <queue>
 #include <vector>
+#include <stack>
+#include <map>
 
 enum edge_state {
 	UNDISCOVERED,
@@ -15,6 +17,7 @@ class graph {
 
 	graph(int n) {
 		vertices = new std::list<int>[n];
+		parent.reserve(n);
 		n_edges = 0;
 		n_vertices = n;
 	}
@@ -35,6 +38,7 @@ class graph {
 
 		state[node] = DISCOVERED;
 		q.push(node);
+		parent[node] = -1;
 
 		while (q.size()) {
 			node = q.front();
@@ -44,9 +48,10 @@ class graph {
 			end = vertices[node].end();
 
 			while (it != end) {
-				if (state[*it] != DISCOVERED) {
+				if (state[*it] == UNDISCOVERED) {
 					q.push(*it);
 					state[*it] = DISCOVERED;
+					parent[*it] = node;
 
 				}
 				process_edge(std::make_tuple(node, *it));
@@ -63,10 +68,56 @@ class graph {
 		dfs_internal(node, state);
 	}
 
+	int shortest_path(int node) {
+		std::stack<int> s;
+		int n;
+
+		s.push(node);
+
+		while (parent[node] != -1) {
+			node = parent[node];
+			s.push(node);
+		}
+
+		std::cout << "Shortest path from root to node " << node << std::endl;
+		std::cout << "^ ";
+
+		n = s.size();
+
+		while (s.size()) {
+			std::cout << s.top() << "->";
+			s.pop();
+		}
+
+		std::cout << "\b\b $" << std::endl;
+		return n-1;
+	}
+
+	void process_node_topological_sort(int node) {
+		sorted.push(node);
+	}
+
+	void topological_sort(int node=0) {
+		process_node_late = true;
+		dfs(node);
+
+		std::cout << "Topological sort" << std::endl;
+
+		while (sorted.size()) {
+			std::cout << "-> " << sorted.top();
+			sorted.pop();
+		}
+
+		std::cout << std::endl;
+	}
+
 	private:
 	std::list<int> *vertices;
+	std::vector<int> parent;
+	std::stack<int> sorted;
 	int n_edges;
 	int n_vertices;
+	bool process_node_late;
 
 	void process_node(int node) {
 		std::cout << "Processing node " << node << std::endl;
@@ -93,9 +144,38 @@ class graph {
 			it++;
 		}
 
+		if (process_node_late)
+			sorted.push(node);
+
 		state[node] = PROCESSED;
 	}
 };
+
+void snake_and_ladder() {
+	std::map<int, int> snl;
+	graph g(30);
+	int i, j, end;
+
+	snl[2] = 21;
+	snl[4] = 7;
+	snl[10] = 25;
+	snl[19] = 28;
+	snl[16] = 3;
+	snl[18] = 6;
+	snl[20] = 8;
+	snl[26] = 0;
+
+	for (i=0; i<30; i++) {
+		for (j=1; j<=6; j++) {
+			end = snl[i+j] ? snl[i+j] : i+j;
+			if(end < 30)
+				g.insert_edge(std::make_tuple(i, end));
+		}
+	}
+
+	g.bfs(0);
+	std::cout << "Min throws " << g.shortest_path(29) << std::endl;
+}
 
 int main(void) {
 	graph g(10);
@@ -112,4 +192,7 @@ int main(void) {
 	g.bfs(0);
 	std::cout << "DFS Traversal" << std::endl;
 	g.dfs(0);
+	g.shortest_path(9);
+
+	snake_and_ladder();
 }
