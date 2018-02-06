f9f0dfe40756ef1558e4927d94533899cf38e5e3
Ashwanth <iamashwanth@gmail.com>
2018-02-06 08:50:53 +0530

    matrix: Fix print diagonal for even sized matrices

--- src/matrix/print_diagonal.cpp
+++ src/matrix/print_diagonal.cpp
@@ -24,7 +24,11 @@ void print_spiral(int A[MAX][MAX]) {
 	while(row != MAX && col != MAX) {
 		std::cout << A[row][col] << " ";
 
-		if (oob(col, col_dir)) {
+		if (oob(col, col_dir) && oob(row, row_dir)) {
+			if (row == 0) row++;
+			else if (col == 0) col++;
+		}
+		else if (oob(col, col_dir)) {
 			row++;
 		} else if (oob(row, row_dir)) {
 			col++;
@@ -34,17 +38,19 @@ void print_spiral(int A[MAX][MAX]) {
 			continue;
 		}
 
+		std::cout << std::endl;
 		row_dir *= -1;
 		col_dir *= -1;
 	}
 }
 
 int main() {
-	int A[MAX][MAX] = {{1,   2,  3,  4,  5},
-			   {6,   7,  8,  9, 10},
+	int A[MAX][MAX] = {{ 1,  2,  3,  4,  5},
+			   { 6,  7,  8,  9, 10},
 			   {11, 12, 13, 14, 15},
 			   {16, 17, 18, 19, 20},
-			   {21, 22, 23, 24, 25},};
+			   {21, 22, 23, 24, 25}};
+
 	print_spiral(A);
 	return 0;
 }
