1449d05cc950736aeda3e42addd792328fe3bac5
Ashwanth <iamashwanth@gmail.com>
2018-02-18 23:00:57 +0530

    Making longest common subsequence more concise

--- src/dynamic_programming/longest_common_subsequence.cpp
+++ src/dynamic_programming/longest_common_subsequence.cpp
@@ -12,48 +12,31 @@ void longest_common_subsequence(char* A, char *B, int m, int n) {
 	std::vector<std::vector<int>> C;
 	C.reserve(m);
 
-	for(int i=0; i<n; i++) C[i].reserve(n);
-
-	int match = 0, max = 0;
-
 	for(int i=0; i<m; i++) {
-		for(int j=0; j<n; j++) {
-			match = (A[i] == B[j]) ? 1 : 0;
 
-			if (match) {
+		C[i].reserve(n);
+
+		for(int j=0; j<n; j++) {
+
+			if (A[i] == B[j]) {
+				std::cout << A[i] << " ";
 				C[i][j] = access_memo(C, i-1, j-1, m, n) + 1;
 			} else {
 				C[i][j] = std::max(access_memo(C, i-1, j, m, n),
 						   access_memo(C, i, j-1, m, n));
 			}
+
 		}
 	}
+
+	std::cout << std::endl;
 
 	for(int i=0; i<m; i++) {
 		for(int j=0; j<n; j++) {
 			std::cout << C[i][j] << " ";
-			max = std::max(C[i][j], max);
 		}
 		std::cout << std::endl;
 	}
-
-	std::cout << std::endl;
-
-	int i = m-1, j = n-1;
-	while (max) {
-		if (C[i][j] == access_memo(C, i-1, j, m, n))
-			i = i-1;
-		else if (C[i][j] == access_memo(C, i, j-1, m, n))
-			j = j-1;
-		else {
-			std::cout << A[i] << " ";
-			max--;
-			i = i-1;
-			j = j-1;
-		}
-	}
-
-	std::cout << std::endl;
 }
 
 int main() {
