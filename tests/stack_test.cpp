6c2ec44d9c97ff681b15d90f7a4908248ea478d8
Ashwanth <iamashwanth@gmail.com>
2018-03-03 00:16:49 +0530

    stack: adding sort routine for stack

--- tests/stack_test.cpp
+++ tests/stack_test.cpp
@@ -22,10 +22,32 @@ int reverse_test(void *data) {
 	return TEST_SUCCESS;
 }
 
+int sort_test(void *data) {
+	std::stack<int> s;
+	s.push(-3);
+	s.push(3);
+	s.push(1);
+	s.push(9);
+	s.push(4);
+	s.push(2);
+
+	sort_stack(s);
+
+	while (s.size()) {
+		std::cout << s.top() << " ";
+		s.pop();
+	}
+
+	std::cout << std::endl;
+
+	return TEST_SUCCESS;
+}
+
 const TestFamily* stack_init() {
     TestFamily *testFamily = new TestFamily("STACK", static_cast<int>(10));
 
     TEST_DEF(REVERSE_TEST, reverse_test);
+    TEST_DEF(SORT_TEST, sort_test);
 
     return testFamily;
 }
