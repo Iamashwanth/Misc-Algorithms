9b9f485f7b4062dbe8944bdb7de62fdb13fbc7de
Ashwanth <iamashwanth@gmail.com>
2018-03-06 23:28:34 +0530

    arrays: largest contiguous sum

---
+++ library/inc/arrays.h
@@ -0,0 +1,8 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+
+#include <vector>
+
+int largest_sum_subarray(std::vector<int> const &v);
+
+#endif
--- tests/test_groups.h
+++ tests/test_groups.h
@@ -6,6 +6,7 @@ extern const TestFamily* bst_init(void);
 extern const TestFamily* list_init(void);
 extern const TestFamily* misc_init(void);
 extern const TestFamily* stack_init(void);
+extern const TestFamily* array_init(void);
 
 typedef const TestFamily* (*InitFunction)(void);
 
@@ -19,6 +20,7 @@ struct TestGroups {
     {.init = list_init},
     {.init = misc_init},
     {.init = stack_init},
+    {.init = array_init},
 };
 
 #endif /* ifndef TESTGROUPS_H */
